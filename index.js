e143abc22d89a0fcc56a3debc4f30f27f2fcf631
federicotempo <federicotempo98@gmail.com>
2025-01-20 17:09:34 -0300

    Modify servefile arguments
    -Deletes contentType argument.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require("fs");
 
 const PORT = 8080;
 
-const serveFile = (filePath, contentType, res) => {
+const serveFile = (filePath, res) => {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("500 Internal Server Error");
     } else {
-      res.writeHead(200, { "Content-Type": contentType });
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(content, "utf-8");
     }
   })
@@ -18,23 +18,22 @@ const serveFile = (filePath, contentType, res) => {
 
 const server = http.createServer((req, res) => {
   let filePath;
-
   switch (req.url) {
     case "/":
       filePath = path.join(__dirname, "index.html");
-      serveFile(filePath, "text/html", res);
+      serveFile(filePath, res);
       break;
     case "/about":
       filePath = path.join(__dirname, "about.html");
-      serveFile(filePath, "text/html", res);
+      serveFile(filePath, res);
       break;
     case "/contact-me":
       filePath = path.join(__dirname, "contact-me.html");
-      serveFile(filePath, "text/html", res);
+      serveFile(filePath, res);
       break;
     default:
       filePath = path.join(__dirname, "404.html");
-      serveFile(filePath, "text/html", res);
+      serveFile(filePath, res);
       break;
   }
 });
